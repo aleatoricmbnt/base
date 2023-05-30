0104eb08de782c87de6fc1388f85092b51ff5b43
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-05-30 23:03:42 +0300

    fixed plan with tfvars

--- CV_tests/plan_with_tfvars/main.tf
+++ CV_tests/plan_with_tfvars/main.tf
@@ -6,5 +6,5 @@ resource "null_resource" "test" {
 
 variable "string" {
   type = string
-  description = "Set relative path to varfile as '..\tfvars\default.tfvars'"
+  description = "Set relative path to varfile as '..\\tfvars\\default.tfvars'"
 }
