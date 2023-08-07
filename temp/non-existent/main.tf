efc438b19ae78209e361672a5dadefe7f25a4757
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-07 14:36:57 +0300

    added inputs to the n-e plan

--- temp/non-existent/main.tf
+++ temp/non-existent/main.tf
@@ -1,3 +1,19 @@
 resource "null_resource" "name" {
   
 }
+
+resource "random_pet" "name" {
+  keepers = {
+    keeper1 = var.one
+    keeper2 = var.two
+  }
+}
+
+variable "one" {
+  default = "some default string 1111111111111111"
+}
+
+variable "two" {
+  type = number
+  default = 222222222222222222222
+}
