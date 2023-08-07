7ad14ce55aaf3280de1962ee7481982f559b4245
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-07 17:15:18 +0300

    added different resource types to the module

--- modules/terraform-submodules-sub1/main.tf
+++ modules/terraform-submodules-sub1/main.tf
@@ -9,3 +9,13 @@ module "terraform-submodules-sub2" {
 module "terraform-submodules-sub3" {
   source = "./tf-sub-sub3"
 }
+
+module "iam_iam-account" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-account"
+  version = "5.28.0"
+  # insert the 1 required variable here
+}
+
+resource "random_pet" "pet" {
+  
+}
