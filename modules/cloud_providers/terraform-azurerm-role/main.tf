030d627f5bb80852ed200af55a23be7601471672
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-10 17:37:16 +0300

    +azuread provider to azure plan // fixes

--- modules/cloud_providers/terraform-azurerm-role/main.tf
+++ modules/cloud_providers/terraform-azurerm-role/main.tf
@@ -24,7 +24,7 @@ provider "azurerm" {
 # ----------------------------------------- AZURERM -----------------------------------------
 # -------------------------------------------------------------------------------------------
 
-resource "azurerm_role_definition" "azure_role" {
+resource "azurerm_role_definition" "azure_role_oidc" {
   name        = "pcfg-test-role"
   scope       = var.scope_subscription-id
   description = "PCFG-TEST This is a custom role created via Terraform"
@@ -35,15 +35,20 @@ resource "azurerm_role_definition" "azure_role" {
   }
 }
 
+resource "azurerm_role_definition" "azure_role_secrets" {
+  name        = "pcfg-test-role"
+  scope       = var.scope_subscription-id
+  description = "PCFG-TEST This is a custom role created via Terraform"
+
+  permissions {
+    actions     = ["*"]
+    not_actions = []
+  }
+  
+  provider = azurerm.secret
+}
+
 variable "scope_subscription-id" {
   type = string
   default = "/subscriptions/957ab5e0-6cf3-4167-89ce-119e4977bf12"
-}
-
-resource "azuread_group" "example" {
-  display_name     = "created-with-terraform"
-  security_enabled = true
-  # providers = {
-  #   azurerm = azurerm.secret
-  # }
 }
