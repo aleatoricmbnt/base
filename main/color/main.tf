eae059eec8751b0b9038cf147930ee249bf53129
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-10 14:35:56 +0300

    added showcolor plan (experimental) // fixes

--- main/color/main.tf
+++ main/color/main.tf
@@ -17,7 +17,7 @@ GREEN=${random_integer.color_list[1].id}
 BLUE=${random_integer.color_list[2].id}
 
 # String to be highlighted
-STRING="I AM COLORED WOW!!!"
+STRING="I AM COLORED AND BOLD WOW!!!"
 
 # ANSI escape codes for bold and RGB color
 BOLD="\033[1m"
