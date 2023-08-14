16cdffe6ed7dba3c22aea27624ef91a3c62c6c53
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-14 22:10:51 +0300

    added test plan TBD // fixes

--- temp/playground/script.sh
+++ temp/playground/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Define RGB color values
-RED=175
-GREEN=31
-BLUE=240
+RED="175"
+GREEN="31"
+BLUE="240"
 
 # ANSI escape codes for bold and RGB color
 BOLD="\033[1m"
@@ -11,4 +11,4 @@ COLOR="\033[38;2;${RED};${GREEN};${BLUE}m"
 RESET="\033[0m"
 
 # Display the highlighted string
-echo -e "$${BOLD}$${COLOR}■■■$${RESET}"
+echo -e "${BOLD}${COLOR}■■■${RESET}"
