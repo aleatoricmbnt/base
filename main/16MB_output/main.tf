ea64af1a58e36fe23cfccba35461353ec82627c5
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-28 09:55:23 +0300

    changed output to short

--- main/16MB_output/main.tf
+++ main/16MB_output/main.tf
@@ -3,7 +3,11 @@ resource "null_resource" "check_state" {
 }
 
 data "local_file" "file_16mb" {
-  filename = "5001.txt"
+  filename = "5001s.txt"
+}
+
+output "short" {
+  value = "some string"
 }
 
 output "output_16mb" {
