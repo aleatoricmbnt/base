ff6af639602bb1fe0c934bb1f4cc0053b0a2acbb
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-28 17:19:27 +0300

    specific size logs // temporary disabled script execution via null resource

--- main/110KB_plus_log/main.tf
+++ main/110KB_plus_log/main.tf
@@ -1,11 +1,11 @@
-resource "null_resource" "generate_file" {
-  provisioner "local-exec" {
-    command = "python 110kb.py"
-  }
-}
+# resource "null_resource" "generate_file" {
+#   provisioner "local-exec" {
+#     command = "python 110kb.py"
+#   }
+# }
 
 resource "null_resource" "display_file" {
-  depends_on = [ null_resource.generate_file ]
+#   depends_on = [ null_resource.generate_file ]
   provisioner "local-exec" {
     command = "cat 110.txt"
   }
--- main/400MB_plus_log/main.tf
+++ main/400MB_plus_log/main.tf
@@ -1,11 +1,11 @@
-resource "null_resource" "generate_file" {
-  provisioner "local-exec" {
-    command = "python 400mb.py"
-  }
-}
+# resource "null_resource" "generate_file" {
+#   provisioner "local-exec" {
+#     command = "python 400mb.py"
+#   }
+# }
 
 resource "null_resource" "display_file" {
-  depends_on = [ null_resource.generate_file ]
+  # depends_on = [ null_resource.generate_file ]
   provisioner "local-exec" {
     command = "cat 409600.txt"
   }
