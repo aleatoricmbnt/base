b022a6744ddab3ef2ed7f8df3142f546d4aa326a
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-28 08:29:01 +0300

    added plan with 16mb output

---
+++ main/16MB_output/main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "check_state" {
+  
+}
+
+data "local_file" "file_16mb" {
+  filename = "5001.txt"
+}
+
+output "output_16mb" {
+  value = data.local_file.file_16mb.content
+}
