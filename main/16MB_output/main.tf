43e836f34b89d568ae29ca6ce7d62737280b17af
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-28 10:31:48 +0300

    deleted short output

--- main/16MB_output/main.tf
+++ main/16MB_output/main.tf
@@ -6,10 +6,6 @@ data "local_file" "file_16mb" {
   filename = "5001s.txt"
 }
 
-output "short" {
-  value = "some string"
-}
-
 output "output_16mb" {
   value = data.local_file.file_16mb.content
 }
