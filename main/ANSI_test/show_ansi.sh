6903384749ba616ee13ac083f8ec206ab1130086
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-29 15:43:29 +0300

    ansi symbols test // added script

---
+++ main/ANSI_test/show_ansi.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "---------------------------------------------------------------------------------------"
+echo "Formatting"
+echo "---------------------------------------------------------------------------------------"
+
+echo "---------------------------------------"
+echo "Bold"
+echo "---------------------------------------"
+echo -e "\033[1mThis is bold text.\nIt spans multiple lines.\nIsn't it impressive?\033[0m"
+
+echo "---------------------------------------"
+echo "Dim"
+echo "---------------------------------------"
+echo -e "\033[2mThis is dim text.\nIt's not as prominent.\nBut it still has its charm.\033[0m"
+
+echo "---------------------------------------"
+echo "Italic"
+echo "---------------------------------------"
+echo -e "\033[3mThis is italic text.\033[0m
+\033[3mIt's slanted and stylish.\033[0m
+\033[3mPerfect for emphasis.\033[0m"
