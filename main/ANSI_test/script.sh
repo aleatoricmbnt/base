45df3651e646567fce03d95744a048eb3920bf30
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-29 16:42:07 +0300

    ansi symbols test // added script // tests

--- main/ANSI_test/script.sh
+++ main/ANSI_test/script.sh
@@ -3,13 +3,20 @@
 
 echo -e "\033[1mThis is bold text.\nIt spans multiple lines.\nIsn't it impressive?\033[0m"
 
+echo " "
+
 echo -e "\033[3mThis is italic text.
 It's slanted and stylish.
 Perfect for emphasis.\033[0m"
 
+echo " "
+
 echo -e "\033[4mThis is underlined text.
 It has a line below.
 Highlighting its importance.\033[0m"
+
+echo " "
+
 
 echo -e "\033[38;5;9mThis text is red.
 Second line also should be red!
