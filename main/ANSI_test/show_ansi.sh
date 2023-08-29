b78bf9d5bc5f7a27d5ba3301e7a75be141f6c98f
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-08-29 16:06:01 +0300

    ansi symbols test // added script // tests

--- main/ANSI_test/show_ansi.sh
+++ main/ANSI_test/show_ansi.sh
@@ -10,13 +10,37 @@ echo "---------------------------------------"
 echo -e "\033[1mThis is bold text.\nIt spans multiple lines.\nIsn't it impressive?\033[0m"
 
 echo "---------------------------------------"
-echo "Dim"
-echo "---------------------------------------"
-echo -e "\033[2mThis is dim text.\nIt's not as prominent.\nBut it still has its charm.\033[0m"
-
-echo "---------------------------------------"
 echo "Italic"
 echo "---------------------------------------"
 echo -e "\033[3mThis is italic text.
 It's slanted and stylish.
 Perfect for emphasis.\033[0m"
+
+
+echo "---------------------------------------"
+echo "Underline"
+echo "---------------------------------------"
+echo -e "\033[4mThis is underlined text.
+It has a line below.
+Highlighting its importance.\033[0m"
+
+echo "---------------------------------------"
+echo "Blink"
+echo "---------------------------------------"
+echo -e "\033[5mThis text is blinking.
+On, off, on, off, on...
+ou can't miss it!\033[0m"
+
+echo "---------------------------------------"
+echo "Reverse"
+echo "---------------------------------------"
+echo -e "\033[7mThis text has reversed colors.
+Foreground and background swap.
+A unique visual twist.\033[0m"
+
+echo "---------------------------------------"
+echo "Hidden"
+echo "---------------------------------------"
+echo -e "\033[8mThis text is hidden.
+Can you see it? Nope, gone!
+A vanishing act.\033[0m"
