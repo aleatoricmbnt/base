61e940ee8d5079fc4beffb739e117b19984290ee
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-09-08 19:17:03 +0300

    added plan to track resource changes

---
+++ main/resource_changes/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    scalr {
+        source = "registry.scalr.io/scalr/scalr"
+    }
+  }
+}
+
+resource "scalr_workspace" "cli-driven" {
+    count = 15
+    name            = "my-workspace-name_${count.index}"
+    environment_id  = "env-v0o2dm5uk9c5qum71"
+}
+
+resource "null_resource" "name" {
+  count = 200
+}
+
+resource "scalr_environment" "test" {
+    count = 10
+    name       = "test-env_${count.index}"
+}
