dc33bf01eb4560bfa6a7f1e6baaf3dd0b3801732
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-10-03 18:25:26 +0300

    commented the sclar provider source string + changed the version to >= 1.0.4

--- main/provider_regression/main.tf
+++ main/provider_regression/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     scalr = {
-      source  = "registry.scalr.io/scalr/scalr"
-      version = "1.0.4"
+      # source  = "registry.scalr.io/scalr/scalr"
+      version = ">= 1.0.4"
     }
   }
 }
