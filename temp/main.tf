111f5cb371d769f799326315ca6e1aedbaa78f3f
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-10-30 22:07:01 +0200

    changed module to the global level one (acc reference)

--- temp/main.tf
+++ temp/main.tf
@@ -1,4 +1,9 @@
-module "integer" {
-	source  = "mshytse.on-prem.scalr.dev/env-v0o4a0pofki2ilc4f/integer/random"
+module "s3bucket" {
+	source  = "mshytse.on-prem.scalr.dev/acc-v0o4a0poe8vnenk6r/s3bucket/aws"
 	version = "0.0.1"
+
+	# Set 1 required variable below.
+
+	# Number of buckets to be created
+ 	quantity = 1
 }
