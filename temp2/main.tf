997dd677a0d09006b5d4cec6bd24a9d6d0fc1278
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-11-02 11:44:00 +0200

    stg check

--- temp2/main.tf
+++ temp2/main.tf
@@ -2,3 +2,8 @@ module "integer" {
 	source  = "aleatoric.main.scalr.dev/acc-ttf6li416m4sbi0/integer/random"
 	version = "0.0.1"
 }
+
+module "role" {
+	source  = "aleatoric.main.scalr.dev/env-v0ns0m539r05815rg/role/azurerm"
+	version = "0.0.2"
+}
