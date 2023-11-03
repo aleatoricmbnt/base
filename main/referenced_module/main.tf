cabfc6a223740793227eb316352e05416eaf3e87
aleatoricmbnt <aleatoricmbnt@gmail.com>
2023-11-03 14:22:54 +0200

    plan to be changed manually every time it needs

--- main/referenced_module/main.tf
+++ main/referenced_module/main.tf
@@ -1,20 +1,4 @@
-module "referenced" {
-  count = var.count-mod
-  source = var.source-mod
-  version = var.version-mod
-}
-
-variable "count-mod" {
-  type = number
-  description = "The number of modules to be created"
-}
-
-variable "source-mod" {
-  type = string
-  description = "The link to your Scalr module"
-}
-
-variable "version-mod" {
-  type = string
-  description = "The version of your Scalr module"
+module "integer" {
+	source  = "test.dependency.testenv.scalr.dev/acc-v0o4jrqmoa4k8hir1/integer/random"
+	version = "0.0.1"
 }
