57e9815d874e56ca7a377c8bea234a7ae1786eb0
Mariia Shytse <aleatoricmbnt@gmail.com>
2023-12-28 14:57:53 +0200

    Update main.tf

--- main/git_https_target/main.tf
+++ main/git_https_target/main.tf
@@ -4,6 +4,17 @@ resource "null_resource" "referenced" {
   }
 }
 
+module "modules_terraform-null-module" {
+	source  = "aleatoric.main.scalr.dev/env-v0ns0m539r05815rg/modules/test//modules/terraform-null-module"
+	version = "0.0.7"
+
+	# Set 1 required variable below.
+
+	# Number of resources to be created
+ 	quantity = 1
+}
+
+
 output "null_resource_id" {
   description = "The `id` of the `null_resource.refrenced` resource in this module."
   value       = null_resource.referenced.id
