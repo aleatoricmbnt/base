2bb76c3d1256232d1f5fd607f265b352cfb4b8a7
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-02-16 17:03:24 +0200

    pwd

---
+++ main/command_pwd/main.tf
@@ -0,0 +1,8 @@
+resource "null_resource" "provisioner" {
+  triggers = {
+    current_time = (formatdate("YYYY-MM-DD_hh:mm:ss",timestamp()))
+  }
+  provisioner "local-exec" {
+    command = "script.sh"
+  }
+}
