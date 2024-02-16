1ef03b233363b6186ed4b0ccf58a56a5a4fc57e8
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-02-16 17:34:11 +0200

    pwd cosmetic change + module/root

--- main/command_pwd/main.tf
+++ main/command_pwd/main.tf
@@ -7,25 +7,55 @@ resource "null_resource" "script_pwd" {
   }
 }
 
-resource "null_resource" "object_pwd" {
-  triggers = {
-    current_time = (formatdate("YYYY-MM-DD_hh:mm:ss",timestamp()))
-  }
-  provisioner "local-exec" {
-    command = "echo ${path.cwd} > echo_result.txt"
-  }
-}
-
 data "local_file" "read_script" {
   depends_on = [null_resource.script_pwd]
   filename   = "./script_result.txt"
 }
 
-data "local_file" "read_echo" {
-  depends_on = [null_resource.object_pwd]
-  filename   = "./echo_result.txt"
+resource "null_resource" "object_pwd" {
+  triggers = {
+    current_time = (formatdate("YYYY-MM-DD_hh:mm:ss",timestamp()))
+  }
+  provisioner "local-exec" {
+    command = "echo ${path.cwd} > object_pwd.txt"
+  }
 }
 
-output "comparison" {
-  value = "script: ${data.local_file.read_script.content}echo: ${data.local_file.read_echo.content}"
+data "local_file" "read_object_pwd" {
+  depends_on = [null_resource.object_pwd]
+  filename   = "./object_pwd.txt"
+}
+
+
+resource "null_resource" "object_module" {
+  triggers = {
+    current_time = (formatdate("YYYY-MM-DD_hh:mm:ss",timestamp()))
+  }
+  provisioner "local-exec" {
+    command = "echo ${path.module} > object_module.txt"
+  }
+}
+
+data "local_file" "read_object_module" {
+  depends_on = [null_resource.object_module]
+  filename   = "./object_module.txt"
+}
+
+resource "null_resource" "object_root" {
+  triggers = {
+    current_time = (formatdate("YYYY-MM-DD_hh:mm:ss",timestamp()))
+  }
+  provisioner "local-exec" {
+    command = "echo ${path.root} > object_root.txt"
+  }
+}
+
+data "local_file" "read_object_root" {
+  depends_on = [null_resource.object_root]
+  filename   = "./object_roote.txt"
+}
+
+
+output "pwd_results" {
+  value = "script: ${data.local_file.read_script.content}object_pwd: ${data.local_file.read_object_pwd.content}object_module: ${data.local_file.read_object_module.content}object_root: ${data.local_file.read_object_root.content}"
 }
