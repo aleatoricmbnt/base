295b154a34bbc418874df19db9da2284da820baf
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-02-16 17:16:44 +0200

    pwd improvements??

--- main/command_pwd/main.tf
+++ main/command_pwd/main.tf
@@ -1,8 +1,31 @@
-resource "null_resource" "provisioner" {
+resource "null_resource" "script_pwd" {
   triggers = {
     current_time = (formatdate("YYYY-MM-DD_hh:mm:ss",timestamp()))
   }
   provisioner "local-exec" {
     command = "./script.sh"
   }
+}
+
+resource "null_resource" "object_pwd" {
+  triggers = {
+    current_time = (formatdate("YYYY-MM-DD_hh:mm:ss",timestamp()))
+  }
+  provisioner "local-exec" {
+    command = "echo ${pwd} > echo_result.txt"
+  }
+}
+
+data "local_file" "read_script" {
+  depends_on = [null_resource.script_pwd]
+  filename   = "./script_result.txt"
+}
+
+data "local_file" "read_echo" {
+  depends_on = [null_resource.echo_result]
+  filename   = "./echo_result.txt"
+}
+
+output "comparison" {
+  value = "script: ${data.local_file.read_script.content} || echo: ${data.local_file.read_echo.content}"
 }
