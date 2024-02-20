0b0d8599d4d5e7dd54c6dd83b98318936c439a2d
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-02-20 19:57:53 +0200

    checking runner permissions // fixes final 100% I SWEAR

--- main/check_runner_permissions/possible.tf
+++ main/check_runner_permissions/possible.tf
@@ -1,9 +1,11 @@
 data "scalr_environment" "possibly_read_env" {
   id         = var.read_env_2_id
+  depends_on = [ null_resource.cat_get_users ]
 }
 
 data "scalr_workspaces" "possibly_read_ws" {
   name = "like:cli"
+  depends_on = [ null_resource.cat_get_users ]
 }
 
 
@@ -11,10 +13,12 @@ resource "null_resource" "possibly_get_state" {
   provisioner "local-exec" {
     command = "curl --request GET --url 'https:///$SCALR_HOSTNAME/api/iacp/v3/state-versions/${var.read_state_id}' --header 'Prefer: profile=preview' --header 'accept: application/vnd.api+json' --header 'authorization: Bearer $${SCALR_TOKEN}'"
   }
+  depends_on = [ null_resource.cat_get_users ]
 }
 
 data "scalr_variables" "possibly_read_vars" {
   keys             = ["key1", "key2", "key3"]
   category         = "terraform" # or shell
   environment_ids = ["${var.read_env_2_id}", "null"]
+  depends_on = [ null_resource.cat_get_users ]
 }
