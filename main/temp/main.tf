233f15e2c1c0ea78bacac86953364d716fcefcab
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-02-26 12:43:57 +0200

    added plan for test (dev-sup) // tests

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -7,21 +7,21 @@ terraform {
   }
 }
 
-# data "scalr_current_account" "account" {}
+data "scalr_current_account" "account" {}
 
-# resource "scalr_environment" "dev" {
-#   name                            = "Development"
-#   account_id                      = data.scalr_current_account.account.id
-#   cost_estimation_enabled         = true
-#   policy_groups                   = []
-#   default_provider_configurations = []
-# }
+resource "scalr_environment" "dev" {
+  name                            = "Development"
+  account_id                      = data.scalr_current_account.account.id
+  cost_estimation_enabled         = true
+  policy_groups                   = []
+  default_provider_configurations = []
+}
 
-# resource "scalr_agent_pool" "dev" {
-#   name           = "agent-dev"
-#   account_id     = data.scalr_current_account.account.id
+resource "scalr_agent_pool" "dev" {
+  name           = "agent-dev"
+  account_id     = data.scalr_current_account.account.id
 #   environment_id = scalr_environment.dev.id
-# }
+}
 
 resource "scalr_agent_pool_token" "dev" {
   agent_pool_id = "apool-v0o94d42k94a6c926" # scalr_agent_pool.dev.id
