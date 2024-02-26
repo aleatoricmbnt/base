e4daf4eadce5f1228b203e792fa77f26790b2e5d
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-02-26 11:59:17 +0200

    added plan for test (dev-sup)

---
+++ main/temp/main.tf
@@ -0,0 +1,20 @@
+data "scalr_current_account" "account" {}
+
+resource "scalr_environment" "dev" {
+  name                            = "Development"
+  account_id                      = data.scalr_current_account.account.id
+  cost_estimation_enabled         = true
+  policy_groups                   = []
+  default_provider_configurations = []
+}
+
+resource "scalr_agent_pool" "dev" {
+  name           = "agent-dev"
+  account_id     = data.scalr_current_account.account.id
+  environment_id = scalr_environment.dev.id
+}
+
+resource "scalr_agent_pool_token" "dev" {
+  agent_pool_id = scalr_agent_pool.dev.id
+  description   = "Token"
+}
