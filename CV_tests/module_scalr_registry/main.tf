5786ab530293df89197c15c6dbb58fe85f11aeec
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-02-27 15:23:38 +0200

    minor fixes

--- CV_tests/module_scalr_registry/main.tf
+++ CV_tests/module_scalr_registry/main.tf
@@ -1,12 +1,12 @@
 module "smth_from_scalr" {
-  source = var.source
-  version = var.version
+  source = var.mod_source
+  version = var.mod_version
 }
 
-variable "version" {
+variable "mod_version" {
   default = "0.0.1"
 }
 
-variable "source" {
+variable "mod_source" {
   default = "my-account.scalr.io/account_name/scalr_dynamic_vpc_dns/aws"
 }
