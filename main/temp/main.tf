6e8e989175ee613d8b4ac2099fb48dda205e0cc2
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-02-27 15:53:45 +0200

    test error

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,29 +1,3 @@
-terraform {
-  required_providers {
-    scalr = {
-      source = "Scalr/scalr"
-      version = "1.8.0"
-    }
-  }
+resource "null_resource" {
+  
 }
-
-data "scalr_current_account" "account" {}
-
-resource "scalr_environment" "dev" {
-  name                            = "Development"
-  account_id                      = data.scalr_current_account.account.id
-  cost_estimation_enabled         = true
-  policy_groups                   = []
-  default_provider_configurations = []
-}
-
-resource "scalr_agent_pool" "dev" {
-  name           = "agent-dev"
-  account_id     = data.scalr_current_account.account.id
-  environment_id = scalr_environment.dev.id
-}
-
-# resource "scalr_agent_pool_token" "dev" {
-#   agent_pool_id = "apool-v0o94d42k94a6c926" # scalr_agent_pool.dev.id
-#   description   = "Token"
-# }
