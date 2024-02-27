6404b26e4628ce2295d85d2dd7bd7e9649b6a52e
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-02-27 22:31:38 +0200

    test modules

--- CV_tests/module_hashicorp_registry/main.tf
+++ CV_tests/module_hashicorp_registry/main.tf
@@ -1,21 +1,21 @@
 module "label" {
     source  = "cloudposse/label/null"
-    version = "0.1.0"
+    version = "0.25.0"
 
     namespace = "qa"
-    # environment = "mshytse"
+    environment = "mshytse"
     stage = "test"
     name = "example"
-    # attributes = ["attr1", "attr2"]
-    # tags = {
-    #     "executor" = "scalr-run"
-    # }
+    attributes = ["attr1", "attr2"]
+    tags = {
+        "executor" = "scalr-run"
+    }
 }
 
 output "string_id" {
   value = module.label.id
 }
 
-# output "string_tags" {
-#   value = module.label.tags
-# }
+output "string_tags" {
+  value = module.label.tags
+}
