d633bed2553cd10114d810aa88340868dcde8d5c
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-03-13 15:44:06 +0200

    added the plan for checking force cancel on the apply stage

---
+++ main/custom_hooks/apply_force_cancel/main.tf
@@ -0,0 +1,10 @@
+resource "null_resource" "name" {
+  triggers = {
+    time = timestamp()
+  }
+  provisioner "local-exec" {
+    command = <<EOT
+    i=1; trap "echo BOOM" TERM SIGTERM; while true; do  echo "Waiting for signal: ${i}s"; i=$((i + 1)); sleep 1; done
+    EOT
+  }
+}
