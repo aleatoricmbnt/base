5aeb5142a7f0ec39609168820dc5c72a84073ced
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-03-14 15:27:08 +0200

    escape resource reference in local-exec command

--- main/apply_force_cancel/main.tf
+++ main/apply_force_cancel/main.tf
@@ -3,6 +3,6 @@ resource "null_resource" "name" {
     time = timestamp()
   }
   provisioner "local-exec" {
-    command = "i=1; trap \"echo BOOM\" TERM SIGTERM; while true; do  echo \"Waiting for signal: ${i}s\"; i=$((i + 1)); sleep 1; done"
+    command = "i=1; trap \"echo BOOM\" TERM SIGTERM; while true; do  echo \"Waiting for signal: $${i}s\"; i=$((i + 1)); sleep 1; done"
   }
 }
