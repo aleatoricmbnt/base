0d951462bd280a44d2d6fe888b1cfba56526d667
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-04-11 12:22:37 +0300

    temp plan

---
+++ main/temp copy/main.tf
@@ -0,0 +1,35 @@
+# resource "random_password" "password" {
+#   length           = 16
+#   special          = var.special_s
+#   lower = var.lower_s
+# }
+
+# variable "special_s" {
+#   type = bool
+#   default = false
+# }
+
+# variable "lower_s" {
+#   default = false
+# }
+
+terraform {
+  required_providers {
+    scalr = {
+      source = "Scalr/scalr"
+    }
+  }
+}
+
+resource "random_pet" "name" {
+  
+}
+
+resource "scalr_environment" "test" {
+  name                            = random_pet.name.id
+  account_id                      = var.acc_id
+}
+
+variable "acc_id" {
+  
+}
