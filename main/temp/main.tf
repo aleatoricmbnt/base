0a6be2b8b67912fd4d94ba66fe28215a667eac42
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-04-11 11:56:23 +0300

    temp plan

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,14 +1,28 @@
-resource "random_password" "password" {
-  length           = 16
-  special          = var.special_s
-  lower = var.lower_s
+# resource "random_password" "password" {
+#   length           = 16
+#   special          = var.special_s
+#   lower = var.lower_s
+# }
+
+# variable "special_s" {
+#   type = bool
+#   default = false
+# }
+
+# variable "lower_s" {
+#   default = false
+# }
+
+terraform {
+  required_providers {
+    scalr = {
+      source = "Scalr/scalr"
+    }
+  }
 }
 
-variable "special_s" {
-  type = bool
-  default = false
-}
 
-variable "lower_s" {
-  default = false
+resource "scalr_environment" "test" {
+  name                            = "test-env"
+  account_id                      = "acc-v0oarthieg22m0a5e"
 }
