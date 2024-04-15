f751cdb4ef75887ad5f80d272329a83a4acb038c
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-04-15 16:58:14 +0300

    temp

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,28 +1,9 @@
-# resource "random_password" "password" {
-#   length           = 16
-#   special          = var.special_s
-#   lower = var.lower_s
-# }
-
-# variable "special_s" {
-#   type = bool
-#   default = false
-# }
-
-# variable "lower_s" {
-#   default = false
-# }
-
-terraform {
-  required_providers {
-    scalr = {
-      source = "Scalr/scalr"
-    }
+resource "null_resource" "test" {
+  triggers = {
+    smth = var.some
   }
 }
 
-
-resource "scalr_environment" "test" {
-  name                            = "some-name"
-  account_id                      = "acc-v0oasu4fau03jio4k"
+variable "some" {
+  default = "account"
 }
