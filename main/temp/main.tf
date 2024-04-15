33c5574e4cf8e4ade5db5706b24614ce2f69fdc9
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-04-15 18:38:20 +0300

    temp

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -8,15 +8,34 @@ terraform {
 
 
 resource "scalr_environment" "test" {
-  name                            = "${var.env-name}"
+  name                            = "account"
   account_id                      = "acc-v0ob25tvb18lmtqbb"
 }
 
 variable "env-name" {
-  
+  sensitive = true
+  default = "account"
+}
+
+resource "null_resource" "name" {
+  triggers = {
+    sens_value = var.env-name
+  }
 }
 
 resource "random_password" "password" {
   length           = 16
   special          = false
 }
+
+resource "random_password" "password2" {
+  length           = 16
+  special          = var.my-var
+}
+
+variable "my-var" {
+  type = bool
+  default = false
+  sensitive = true
+}
+
