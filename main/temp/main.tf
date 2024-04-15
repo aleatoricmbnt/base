eacb3aeea22efd4dcd9b046916a0798453523cd9
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-04-15 17:12:29 +0300

    temp

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -11,3 +11,7 @@ resource "scalr_environment" "test" {
   name                            = "${var.env-name}test"
   account_id                      = "acc-v0ob25tvb18lmtqbb"
 }
+
+variable "env-name" {
+  
+}
