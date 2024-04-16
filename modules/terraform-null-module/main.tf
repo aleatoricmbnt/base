a79b996a1dce6dd724b21e03fc207cf4f303f0df
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-04-16 18:23:48 +0300

    temp

--- modules/terraform-null-module/main.tf
+++ modules/terraform-null-module/main.tf
@@ -9,3 +9,9 @@ variable "quantity" {
   type        = number
   description = "Number of resources to be created"
 }
+
+resource "random_pet" "name" {
+  keepers = {
+    null_id = null_resource.test[1].id
+  }
+}
