72c548847efaff8fc5c019e344283efd360bd3da
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-05-15 13:03:35 +0300

    fixed git https template

--- config_tests/module_sources/module_https_github_com/main.tf
+++ config_tests/module_sources/module_https_github_com/main.tf
@@ -1,5 +1,10 @@
 module "referencing" {
   source = "git::https://github.com/aleatoricmbnt/base.git//main/git_https_target"
+  custom_var = var.smth
+}
+
+variable "smth" {
+  
 }
 
 output "referencing_module_id_from_output" {
