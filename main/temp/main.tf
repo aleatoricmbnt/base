ec19aa2c26debb4f0a477ea58681ba8087fb3622
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-05-17 22:37:46 +0300

    temp

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,18 +1,9 @@
-variable "map_object" {
-  type = map(object({
-    cidr     = string
-    zone     = string
-    tags     = map(string)
-    cidr_app = string
-    gw_app   = string
-    cidr_db  = string
-    gw_db    = string
-  }))
+variable "string_with_symbols" {
+  
 }
 
-resource "null_resource" "map_object" {
-  provisioner "local-exec" {
-    command = "echo ${var.map_object.AIMS.cidr}"
+resource "null_resource" "name" {
+  triggers = {
+    trigger = var.string_with_symbols
   }
-  triggers = var.map_object.AIMS.tags
 }
