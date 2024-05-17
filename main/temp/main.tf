65b881ff21a53f6ecd96f06ec09acf86eefb23b3
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-05-17 20:14:17 +0300

    temp

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,14 +1,22 @@
-variable "nullable" {
-  type     = string
-  default = null
+# map (object)
+
+variable "map_object" {
+  type = map(object({
+    cidr     = string
+    zone     = string
+    tags     = map(string)
+    cidr_app = string
+    gw_app   = string
+    cidr_db  = string
+    gw_db    = string
+  }))
 }
 
-resource "null_resource" "name" {
-  triggers = {
-    trigger = var.nullable
+resource "null_resource" "map_object" {
+  provisioner "local-exec" {
+    command = "echo ${var.map_object.AIMS.cidr}"
   }
-}
-
-output "nullable_output" {
-  value = var.nullable
+  triggers = {
+    time = timestamp()
+  }
 }
