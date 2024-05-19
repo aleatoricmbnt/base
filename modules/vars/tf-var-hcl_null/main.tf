7ec2810f5aa8bd82b3dea59d1ac8d246efe374d2
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-05-19 19:32:40 +0300

    null vars plan

--- modules/vars/tf-var-hcl_null/main.tf
+++ modules/vars/tf-var-hcl_null/main.tf
@@ -1,14 +1,80 @@
-variable "nullable" {
+variable "not-set-hcl" {
   type     = string
   default = null
 }
 
-resource "null_resource" "name" {
+variable "not-set-non-hcl" {
+  type     = string
+  default = null
+}
+
+variable "null-hcl" {
+  type     = string
+  default = null
+}
+
+variable "null-non-hcl" {
+  type     = string
+  default = null
+}
+
+variable "null-default" {
+  type     = string
+  default = null
+}
+
+variable "null-varfile" {
+  type     = string
+  default = null
+}
+
+variable "empty-string-varfile" {
+  type     = string
+  default = null
+}
+
+####
+# RESOURCES
+####
+
+resource "null_resource" "not-set-hcl" {
   triggers = {
-    trigger = var.nullable
+    trigger = var.not-set-hcl
   }
 }
 
-output "nullable_output" {
-  value = var.nullable
+resource "null_resource" "not-set-non-hcl" {
+  triggers = {
+    trigger = var.not-set-non-hcl
+  }
+}
+
+resource "null_resource" "null-hcl" {
+  triggers = {
+    trigger = var.null-hcl
+  }
+}
+
+resource "null_resource" "null-non-hcl" {
+  triggers = {
+    trigger = var.null-non-hcl
+  }
+}
+
+resource "null_resource" "null-default" {
+  triggers = {
+    trigger = var.null-default
+  }
+}
+
+resource "null_resource" "null-varfile" {
+  triggers = {
+    trigger = var.null-varfile
+  }
+}
+
+resource "null_resource" "empty-string-varfile" {
+  triggers = {
+    trigger = var.empty-string-varfile
+  }
 }
