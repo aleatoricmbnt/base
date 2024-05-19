eac3f097f1cf3537174db0280bca404b1ea86a58
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-05-19 16:27:26 +0300

    temp // plan for nullable var

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,9 +1,14 @@
-variable "string_with_symbols" {
-  
+variable "nullable" {
+  type     = string
+  default = null
 }
 
 resource "null_resource" "name" {
   triggers = {
-    trigger = var.string_with_symbols
+    trigger = var.nullable
   }
+}
+
+output "nullable_output" {
+  value = var.nullable
 }
