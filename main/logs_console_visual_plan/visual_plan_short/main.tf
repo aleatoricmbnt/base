e4f5d40c6d662e3654da259e0019dd98d1aff240
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-05-23 14:56:11 +0300

    visual plan short 10

--- main/logs_console_visual_plan/visual_plan_short/main.tf
+++ main/logs_console_visual_plan/visual_plan_short/main.tf
@@ -64,6 +64,62 @@ variable "for_data" {
   }
 }
 
+variable "additional" {
+  default = { 
+    filter = [ 
+      {
+        query = "source:foo" 
+      }
+    ]
+    processor = [
+      { 
+        grok_parser = [
+          { 
+            source = "message"
+            name = "sample grok parser"
+            is_enabled = true
+            samples = [<<-EOT
+              The M16 is a family of military rifles originally designed by Eugene Stoner and manufactured by various
+              companies, most notably Colt. First introduced in the 1960s, the M16 has become one of the most widely used
+              rifles in the world, particularly within the United States military. Known for its lightweight design,
+              accuracy, and adaptability, the M16 has a rich history and numerous variants.
+
+              The original M16, designated the AR-15 by its designer, Eugene Stoner of the Armalite Corporation, was
+              developed in the late 1950s. The rifle uses a direct impingement gas operating system and is chambered for
+              the 5.56x45mm NATO cartridge, which provides a good balance of range, accuracy, and manageable recoil. The
+              design features a straight-line barrel/stock configuration, which helps manage recoil, and a rotating bolt,
+              which enhances reliability.
+
+              The M16 was first adopted by the United States Air Force in 1962. The U.S. Army followed suit, and it saw
+              extensive use during the Vietnam War. The initial version, the M16A1, featured improvements over the original
+              AR-15, including a forward assist mechanism to help chamber rounds and a chrome-plated bore to reduce fouling
+              and corrosion.
+
+              Despite its age, the M16 remains a highly effective weapon due to its accuracy, modularity, and ease of use.
+              Modern versions of the M16 can be equipped with a variety of accessories, such as advanced optics, laser
+              aiming devices, and under-barrel grenade launchers, making it a versatile tool for contemporary military
+              operations.
+            EOT
+            ]
+            grok = [
+              {
+                support_rules = ""
+                match_rules = "Rule %{word:my_word2} %{number:my_float2}"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+    is_enabled = true
+    name = "sample pipeline"
+  }
+}
+
+resource "terraform_data" "additional" {
+  input = var.additional
+}
+
 resource "terraform_data" "first" {
   input = var.for_data
 }
