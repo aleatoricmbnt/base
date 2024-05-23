ee438b6e848ec26bcf7b4e7087da70db2b375154
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-05-23 18:00:23 +0300

    module sources test

--- config_tests/module_sources/module_github_com_with_subdir/main.tf
+++ config_tests/module_sources/module_github_com_with_subdir/main.tf
@@ -1,5 +1,10 @@
 module "referencing" {
   source = "github.com/aleatoricmbnt/base.git//main/git_https_target"
+  custom_var = var.required
+}
+
+variable "required" {
+  default = "value"
 }
 
 output "referencing_module_id_from_output" {
