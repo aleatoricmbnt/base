017f58eb585019d64e0431d0cdf49ba2a9f42aa4
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-06-25 18:17:14 +0300

    added indexes plan

---
+++ main/indexes/main.tf
@@ -0,0 +1,36 @@
+locals {
+  map =  {
+    some_long_string = 10
+    even_much_longer_string = 20
+    the_string_as_hard_as_a_day_and_as_long_as_the_whole_week_without_beer = 30
+  }
+}
+
+resource "random_password" "test" {
+  for_each = local.map
+  length = each.value
+}
+
+locals {
+  triggers =  {
+    0 = "one"
+    1 = "two"
+    2 = "three"
+  }
+}
+
+resource "null_resource" "test" {
+  for_each = local.triggers
+  triggers = {
+    some_trigger = each.value
+  }
+}
+
+resource "terraform_data" "test" {
+  input = "blah-blah-blah"
+}
+
+resource "terraform_data" "triggered_by_test" {
+  input = "my input"
+  triggers_replace = terraform_data.test
+}
