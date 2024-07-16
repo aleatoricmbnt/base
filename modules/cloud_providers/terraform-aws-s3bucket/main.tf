7119c5faacdc6a277993462e549d8327ae397c6c
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-07-16 19:17:54 +0300

    added big CE

--- modules/cloud_providers/terraform-aws-s3bucket/main.tf
+++ modules/cloud_providers/terraform-aws-s3bucket/main.tf
@@ -48,3 +48,110 @@ resource "scalr_workspace" "cli-driven" {
 data "scalr_current_run" "this" {
   
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+resource "aws_instance" "ubuntu" {
+  count = 149
+   provisioner "local-exec" {
+    command = "env"
+  }
+  #ami                         = var.win_ami
+  ami           = data.aws_ami.ubuntu.id
+  #instance_type = var.instance_type1
+ instance_type = sensitive("t2.nano")
+  subnet_id     = var.subnet
+  tags          = merge({ "Name" = format("k.kotov-test -> %s -> %s", substr("🤔🤷", 0, 1), data.aws_ami.ubuntu.name) }, var.tags)
+  timeouts {
+    create = "9m"
+    delete = "15m"
+  }
+}
+
+
+
+resource "aws_ebs_volume" "ebs-volume" {
+  availability_zone = "us-east-1a"
+        size = 1000000
+    type = "gp2"
+    }
+
+
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    sid       = "AllowFullS3Access"
+    actions   = ["s3:ListAllMyBuckets"]
+    resources = ["*"]
+  }
+}
+
+
+resource "aws_iam_role" "demo-node" {
+  name = "terraform-eks-demo-node"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.demo-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.demo-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.demo-node.name
+}
+
+resource "aws_iam_instance_profile" "demo-node" {
+  name = "terraform-eks-demo"
+  role = aws_iam_role.demo-node.name
+}
+
+resource "aws_security_group" "demo-node" {
+  name        = "terraform-eks-demo-node"
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = "vpc-596aa03e"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #tags = "${
+   # map(
+    # "Name", "terraform-eks-demo-node",
+   #"kubernetes.io/cluster/${var.cluster-name}", "owned",
+   # )
+  #}"
+}
