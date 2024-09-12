b0c83d5cb06c4d7b63eb12acffae53e9831c6f90
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-09-12 11:28:15 +0300

    support tests

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,8 +1,32 @@
-module "modules_terraform-null-module" {
-	source  = "aleatoric.main.scalr.dev/env-v0ns0m539r05815rg/modules/test//modules/terraform-null-module"
-	version = "0.0.9"
+variable "scalr_account_id" {
+  type        = string
+  description = "Account ID to use"
 }
 
-variable "quantity" {
-  default = "2"
+variable "scalr_variable" {
+  type = map(object({
+    key            = optional(string)
+    hcl            = optional(string)
+    value          = optional(string)
+    category       = optional(string)
+    sensitive      = optional(string)
+    description    = optional(string)
+    workspace_id   = optional(string)
+    environment_id = optional(string)
+  }))
+  default     = null
+  description = "Environment level Scalr variable to provision"
 }
+
+resource "scalr_variable" "this" {
+  for_each       = var.scalr_variable
+  key            = each.key
+  hcl            = each.value.hcl
+  value          = each.value.value
+  category       = "terraform"
+  sensitive      = each.value.sensitive
+  account_id     = var.scalr_account_id
+  description    = each.value.description
+  workspace_id   = each.value.workspace_id
+  environment_id = each.value.environment_id
+}  
