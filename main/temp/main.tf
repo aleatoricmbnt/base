4cb4f9c449b8492ba05caa083ad32c19fd5ea0b1
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-10-03 12:28:07 +0300

    temp

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,43 +1,65 @@
-terraform {
-  required_providers {
-    scalr = {
-      source  = "Scalr/scalr"
-      version = "2.1.0"
+# terraform {
+#   required_providers {
+#     scalr = {
+#       source  = "Scalr/scalr"
+#       version = "2.1.0"
+#     }
+#   }
+# }
+
+# provider "scalr" {}
+
+# variable "scalr_account_id" {
+#   type        = string
+#   description = "Account ID to use"
+# }
+
+# variable "scalr_variable" {
+#   type = map(object({
+#     key            = optional(string)
+#     hcl            = optional(string)
+#     value          = optional(string)
+#     category       = optional(string)
+#     sensitive      = optional(string)
+#     description    = optional(string)
+#     workspace_id   = optional(string)
+#     environment_id = optional(string)
+#   }))
+#   default     = null
+#   description = "Environment level Scalr variable to provision"
+# }
+
+# resource "scalr_variable" "this" {
+#   for_each       = var.scalr_variable
+#   key            = each.key
+#   hcl            = each.value.hcl
+#   value          = each.value.value
+#   category       = "terraform"
+#   sensitive      = each.value.sensitive
+#   account_id     = var.scalr_account_id
+#   description    = each.value.description
+#   workspace_id   = each.value.workspace_id
+#   environment_id = each.value.environment_id
+# }  
+
+data "terraform_remote_state" "wait" {
+  backend = "remote"
+
+  config = {
+    hostname = "aleatoric.main.scalr.dev"
+    organization = "env-v0od4sfanb18tjhh8"
+    workspaces = {
+      name = "lw"
     }
   }
+
+  depends_on = [ terraform_data.this ]
 }
 
-provider "scalr" {}
-
-variable "scalr_account_id" {
-  type        = string
-  description = "Account ID to use"
+resource "terraform_data" "this" {
+  input = timestamp()
 }
 
-variable "scalr_variable" {
-  type = map(object({
-    key            = optional(string)
-    hcl            = optional(string)
-    value          = optional(string)
-    category       = optional(string)
-    sensitive      = optional(string)
-    description    = optional(string)
-    workspace_id   = optional(string)
-    environment_id = optional(string)
-  }))
-  default     = null
-  description = "Environment level Scalr variable to provision"
+output "remote_outputs" {
+  value = data.terraform_remote_state.wait.outputs
 }
-
-resource "scalr_variable" "this" {
-  for_each       = var.scalr_variable
-  key            = each.key
-  hcl            = each.value.hcl
-  value          = each.value.value
-  category       = "terraform"
-  sensitive      = each.value.sensitive
-  account_id     = var.scalr_account_id
-  description    = each.value.description
-  workspace_id   = each.value.workspace_id
-  environment_id = each.value.environment_id
-}  
