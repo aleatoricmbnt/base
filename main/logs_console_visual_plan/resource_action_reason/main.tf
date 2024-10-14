9065ef91ecf8f882e5fdf898187dde11009cd225
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-10-14 17:33:58 +0300

    resource action plan

---
+++ main/logs_console_visual_plan/resource_action_reason/main.tf
@@ -0,0 +1,40 @@
+resource "terraform_data" "with_updatable_input" {
+  input = var.update_input
+}
+
+resource "terraform_data" "with_updatable_trigger" {
+  triggers_replace = var.update_trigger
+}
+
+resource "null_resource" "depends_on_updatable_trigger_resource" {
+  depends_on = [ terraform_data.with_updatable_trigger ]
+}
+
+resource "random_password" "updatable_map_keys_with_for_each_expression" {
+  for_each = var.update_map
+  length = each.value
+}
+
+resource "null_resource" "to_be_replaced_manually" {
+  
+}
+
+resource "terraform_data" "updatable_count" {
+  count = var.update_count
+}
+
+resource "terraform_data" "to_be_commented_resource" {
+  # comment me when needed
+}
+
+resource "terraform_data" "deletable_count" {
+  count = var.update_count # delete me when needed
+}
+
+module "to_be_commented_module" {
+  source = "github.com/aleatoricmbnt/flat/" # comment me when needed
+}
+
+resource "terraform_data" "to_be_tainted" {
+  # taint me when needed
+}
---
+++ main/logs_console_visual_plan/resource_action_reason/variables.tf
@@ -0,0 +1,19 @@
+variable "update_input" {
+  default = "default value"
+}
+
+variable "update_trigger" {
+  default = "default value"
+}
+
+variable "update_map" {
+  default = {
+    some_long_string = 10
+    even_much_longer_string = 20
+    the_string_as_hard_as_a_day_and_as_long_as_the_whole_week_without_beer = 30
+  }
+}
+
+variable "update_count" {
+  default = 3
+}
