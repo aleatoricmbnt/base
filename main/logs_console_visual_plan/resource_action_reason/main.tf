da21c84bc1c27e51e933dff80c850566687a84f2
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-10-14 17:38:56 +0300

    resource action plan 2

--- main/logs_console_visual_plan/resource_action_reason/main.tf
+++ main/logs_console_visual_plan/resource_action_reason/main.tf
@@ -23,17 +23,17 @@ resource "terraform_data" "updatable_count" {
   count = var.update_count
 }
 
-resource "terraform_data" "to_be_commented_resource" {
-  # comment me when needed
-}
+# resource "terraform_data" "to_be_commented_resource" {
+#   # comment me when needed
+# }
 
 resource "terraform_data" "deletable_count" {
-  count = var.update_count # delete me when needed
+  # count = var.update_count # delete me when needed
 }
 
-module "to_be_commented_module" {
-  source = "github.com/aleatoricmbnt/flat/" # comment me when needed
-}
+# module "to_be_commented_module" {
+#   source = "github.com/aleatoricmbnt/flat/" # comment me when needed
+# }
 
 resource "terraform_data" "to_be_tainted" {
   # taint me when needed
