595ee4170d00062534275d3c27c931141082677b
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-10-14 17:46:06 +0300

    resource action plan 4

--- main/logs_console_visual_plan/resource_action_reason/main.tf
+++ main/logs_console_visual_plan/resource_action_reason/main.tf
@@ -6,8 +6,8 @@ resource "terraform_data" "with_updatable_trigger" {
   triggers_replace = var.update_trigger
 }
 
-resource "null_resource" "depends_on_updatable_trigger_resource" {
-  depends_on = [ terraform_data.with_updatable_trigger ]
+resource "terraform_data" "depends_on_updatable_trigger_resource" {
+  triggers_replace = [terraform_data.with_updatable_trigger]
 }
 
 resource "terraform_data" "updatable_map_keys_with_for_each_expression" {
@@ -23,17 +23,17 @@ resource "terraform_data" "updatable_count" {
   count = var.update_count
 }
 
-# resource "terraform_data" "to_be_commented_resource" {
-#   # comment me when needed
-# }
-
-resource "terraform_data" "deletable_count" {
-  # count = var.update_count # delete me when needed
+resource "terraform_data" "to_be_commented_resource" {
+  # comment me when needed
 }
 
-# module "to_be_commented_module" {
-#   source = "github.com/aleatoricmbnt/flat/" # comment me when needed
-# }
+resource "terraform_data" "deletable_count" {
+  count = var.update_count # delete me when needed
+}
+
+module "to_be_commented_module" {
+  source = "./subdir" # comment me IN THE MODULE REPO when needed
+}
 
 resource "terraform_data" "to_be_tainted" {
   # taint me when needed
