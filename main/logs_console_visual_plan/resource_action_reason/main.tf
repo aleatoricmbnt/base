78d0d8dd226745b59eb60b098ac37a597d1eeb7e
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-10-14 18:56:46 +0300

    resource action plan 10

--- main/logs_console_visual_plan/resource_action_reason/main.tf
+++ main/logs_console_visual_plan/resource_action_reason/main.tf
@@ -42,3 +42,19 @@ resource "random_string" "name" {
 resource "terraform_data" "to_be_tainted" {
   # taint me when needed
 }
+
+variable "revision" {
+  default = 1
+}
+
+resource "terraform_data" "replacement" {
+  input = var.revision
+}
+
+# This resource has no convenient attribute which forces replacement,
+# but can now be replaced by any change to the revision variable value.
+resource "example_database" "test" {
+  lifecycle {
+    replace_triggered_by = [terraform_data.replacement]
+  }
+}
