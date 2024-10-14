8a3a5920e3c11ad412c9659b1cb9e404d99e0fb4
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-10-14 18:07:45 +0300

    resource action plan 6

--- main/logs_console_visual_plan/resource_action_reason/subdir/main.tf
+++ main/logs_console_visual_plan/resource_action_reason/subdir/main.tf
@@ -1,3 +1,7 @@
-# resource "random_pet" "module_pet" {
-  
-# }
+resource "random_pet" "module_pet" {
+  count = 2
+}
+
+module "nested" {
+  source = "./nested"
+}
---
+++ main/logs_console_visual_plan/resource_action_reason/subdir/nested/main.tf
@@ -0,0 +1,3 @@
+resource "random_string" "name" {
+  length = 12
+}
