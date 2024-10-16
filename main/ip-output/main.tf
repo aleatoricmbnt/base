47c15de5e963d554dc661de53a7116c193440e03
Mariia Shytse <aleatoricmbnt@gmail.com>
2024-10-16 13:16:51 +0300

    null_resource -> terraform_data

--- main/ip-output/main.tf
+++ main/ip-output/main.tf
@@ -1,19 +1,15 @@
-resource "null_resource" "check_ip" {
-  triggers = {
-    current_time = timestamp()
-  }
+resource "terraform_data" "check_ip" {
+  triggers_replace = timestamp()
   provisioner "local-exec" {
     command = "curl -s https://ifconfig.me/ip > ip.txt"
   }
 }
 
 data "local_file" "read_ip" {
-  depends_on = [null_resource.check_ip]
+  depends_on = [terraform_data.check_ip]
   filename   = "./ip.txt"
 }
 
 output "scalr_ip" {
   value = "Current instance IP is: ${data.local_file.read_ip.content}"
 }
-
-# comment
