3b9519c6d3dbd265c01bfc326e0d12232de32ee6
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-12-16 15:35:36 +0200

    added long inputs/outputs descriptions to the long module

--- modules/terraform-long-gyk6lofu95wnsersaw46m67kfg6oaoo2z9f0rw1aav1tsdt0zb87rxxeu6y4r48z/main.tf
+++ modules/terraform-long-gyk6lofu95wnsersaw46m67kfg6oaoo2z9f0rw1aav1tsdt0zb87rxxeu6y4r48z/main.tf
@@ -4,7 +4,7 @@ resource "random_pet" "pet" {
   }
 }
 
-resource "random_pet" "pet_2" {
+resource "random_pet" "pet_2_name_long_so_it_would_be_truncated_on_some_resolutions_and_would_allow_to_check_if_scalr_handles_this_correctly" {
   count = var.pet_count
   keepers = {
     timestamp = timestamp()
