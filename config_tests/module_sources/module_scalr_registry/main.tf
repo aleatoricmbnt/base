0c1790231e6fbb8709e771172cced76432c299ec
Mariia Shytse <aleatoricmbnt@gmail.com>
2025-01-22 13:48:28 +0200

    test scalr registry modules with checkov

--- config_tests/module_sources/module_scalr_registry/main.tf
+++ config_tests/module_sources/module_scalr_registry/main.tf
@@ -1,4 +1,14 @@
-module "smth_from_scalr" {
-  source = "test.perf.testenv.scalr.dev/env-v0o95n88mapt3ls24/integer/random"
-  version = "0.0.1"
+module "registry" {
+	source  = "aleatoric.main.scalr.dev/env-v0ns0m539r05815rg/registry/local"
+	version = "0.0.1"
+}
+
+module "module" {
+	source  = "aleatoric.main.scalr.dev/acc-ttf6li416m4sbi0/module/null"
+	version = "1.0.0"
+
+	# Set 1 required variable below.
+
+	# Number of resources to be created
+ 	quantity = 1
 }
