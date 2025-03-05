d5cb63a9fc952b9731245540adc50d6fb3d53bd9
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-03-05 18:38:41 +0200

    added plan with data source that executes script

---
+++ main/command_execution_during_plan/main.tf
@@ -0,0 +1,10 @@
+resource "null_resource" "name" {
+  triggers = {
+    time = timestamp()
+  }
+}
+
+data "external" "example" {
+  program = ["bash", "./script.sh"]
+  # depends_on = [ null_resource.name ]
+}
