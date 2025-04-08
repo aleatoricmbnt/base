3371758238242a06066f567e2f84ecc3290d14bb
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-04-08 14:17:52 +0300

    Added required_providers {} block to the root module

--- main/local_call_scalr_cli_module/main.tf
+++ main/local_call_scalr_cli_module/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    scalr = {
+      source  = "registry.scalr.io/scalr/scalr"
+    }
+  }
+}
+
 module "scalr_prod_module_2_5_0" {
   source = "../../modules/cloud_providers/terraform-scalr-cli"
 }
