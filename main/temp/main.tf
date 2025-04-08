ba7758d685013f8516ffcec4c5a1df3207064751
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-04-08 14:05:35 +0300

    added explicit version to the scalr-cli module

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,3 +1,23 @@
 module "smth_from_git" {
   source = "github.com/aleatoricmbnt/flat/"
 }
+
+
+resource "terraform_data" "multiple_triggers" {
+  triggers_replace  = [ var.list_of_tags, var.nested_object ]
+}
+
+variable "list_of_tags" {
+  default = {
+    "tag.with.dot" = "s:://"
+    "slash/suffix" = "f"
+  }
+}
+
+variable "nested_object" {
+  default = {
+    upper_level = {
+      "smth.w.dot.and.hash#" = 123
+    }
+  }
+}
--- modules/cloud_providers/terraform-scalr-cli/main.tf
+++ modules/cloud_providers/terraform-scalr-cli/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     scalr = {
       source  = "registry.scalr.io/scalr/scalr"
+      version = "2.5.0"
     }
   }
 }
