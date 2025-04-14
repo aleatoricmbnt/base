b549e7c9d0163ba3064719443871b769f1747494
Mariia Shytse <aleatoricmbnt@gmail.com>
2025-04-14 17:35:03 +0300

    Update main.tf

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,25 +1,5 @@
-module "smth_from_git" {
-  source = "github.com/aleatoricmbnt/flat/"
-}
-
-
-resource "terraform_data" "multiple_triggers" {
-  triggers_replace  = [ var.list_of_tags, var.nested_object ]
-}
-
-variable "list_of_tags" {
-  default = {
-    "tag.with.dot" = "s:://"
-    "slash/suffix" = "f"
+resource "random_pet" "run_from_vcs" {
+  keepers = {
+    timestamp = timestamp()
   }
 }
-
-variable "nested_object" {
-  default = {
-    upper_level = {
-      "smth.w.dot.and.hash#" = 123
-    }
-  }
-}
-
-# PR comment
