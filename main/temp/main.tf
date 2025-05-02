ce8592d58e3e5e1523a2513214e0521b682e9af0
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-05-02 19:33:25 +0300

    added plan to debug autotest 3

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,7 +1,7 @@
 terraform {
     required_providers {
         scalr = {
-            source = "registry.scalr.dev/scalr/scalr"
+            source = "registry.main.scalr.dev/scalr/scalr"
             version= "1.0.0-rc-master"
         }
     }
