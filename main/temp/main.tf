b21db214c7aee31cea198ac3ce4a870ec6607319
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-05-20 12:53:43 +0300

    changed temp

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,20 +1,24 @@
-locals {
-  config  = yamldecode(file("${path.module}/${var.environment}.yaml"))
-  regions = local.config.regions
-}
+# locals {
+#   config  = yamldecode(file("${path.module}/${var.environment}.yaml"))
+#   regions = local.config.regions
+# }
 
-provider "aws" {
-  for_each = local.regions
-  region   = each.key
-  alias    = "by_region"
-}
+# provider "aws" {
+#   for_each = local.regions
+#   region   = each.key
+#   alias    = "by_region"
+# }
 
-resource "aws_s3_bucket" "alfiias_bucket" {
-  for_each = local.regions
-  provider = aws.regions[each.key]
-  bucket = "alfiia-test-s3-${var.environment}-${each.key}" # 
-  tags = {
-    Environment = var.environment
-    Region      = each.key
-  }
+# resource "aws_s3_bucket" "alfiias_bucket" {
+#   for_each = local.regions
+#   provider = aws.regions[each.key]
+#   bucket = "alfiia-test-s3-${var.environment}-${each.key}" # 
+#   tags = {
+#     Environment = var.environment
+#     Region      = each.key
+#   }
+# }
+
+resource "terraform_data" "this" {
+  triggers_replace = timestamp()
 }
