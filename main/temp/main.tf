c38156a86b215ef6885ce6fd5cabe8f0f484b9cc
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-05-26 13:35:10 +0300

    plan to test state rollback

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -19,6 +19,50 @@
 #   }
 # }
 
-resource "terraform_data" "this" {
-  triggers_replace = timestamp()
+terraform {
+  required_providers {
+    scalr = {
+      source = "Scalr/scalr"
+    }
+  }
+}
+
+resource "scalr_workspace" "vcs-driven" {
+  name            = replace(var.workdir, "/", "_")
+  environment_id  = var.env_id
+
+  auto_apply  = var.auto_apply
+  auto_queue_runs = var.auto_queue_runs
+
+  vcs_provider_id = var.vcs_id
+  working_directory = var.workdir
+
+  vcs_repo {
+    identifier       = var.vcs_identifier
+    branch           = "master"
+  }
+}
+
+variable "env_id" {
+  description = "ID  opf the env to create workspace in"
+}
+
+variable "vcs_id" {
+  description = "ID of the VCS provider to create workspace from"
+}
+
+variable "workdir" {
+  description = "Working directory of the workspace"
+}
+
+variable "vcs_identifier" {
+  description = "`org/repo` used as a source for workspace"
+}
+
+variable "auto_apply" {
+  description = "Boolean value for the Auto-apply setting of the workspace"
+}
+
+variable "auto_queue_runs" {
+  description = "Vvalue for the Auto-queue runs setting of the workspace"
 }
