993b979504afea7d96efd5edb2910d217a023360
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-07-02 18:02:34 +0300

    Changed module from scalr registry

--- config_tests/module_sources/module_scalr_registry/main.tf
+++ config_tests/module_sources/module_scalr_registry/main.tf
@@ -1,14 +1,4 @@
-module "registry" {
-	source  = "aleatoric.main.scalr.dev/env-v0ns0m539r05815rg/registry/local"
+module "integer" {
+	source  = "aleatoric.main.scalr.dev/acc-ttf6li416m4sbi0/integer/random"
 	version = "0.0.1"
-}
-
-module "module" {
-	source  = "aleatoric.main.scalr.dev/acc-ttf6li416m4sbi0/module/null"
-	version = "1.0.0"
-
-	# Set 1 required variable below.
-
-	# Number of resources to be created
- 	quantity = 1
 }
