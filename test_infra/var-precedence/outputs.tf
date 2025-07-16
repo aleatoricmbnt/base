c640557a8f8350e4508d187e7032eac8578dde3b
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-07-16 17:27:22 +0300

    test plan with infra to deploy // fix

--- test_infra/var-precedence/outputs.tf
+++ test_infra/var-precedence/outputs.tf
@@ -11,9 +11,9 @@ output "workspace_name" {
 output "variables_created" {
   description = "Variables created for precedence testing"
   value = {
-    account_shell       = var.create_account_shell_variable > 0 ? scalr_variable.account_shell[0].value : "not created"
-    environment_shell   = var.create_environment_shell_variable > 0 ? scalr_variable.environment_shell[0].value : "not created"
-    workspace_shell     = var.create_workspace_shell_variable > 0 ? scalr_variable.workspace_shell[0].value : "not created"
-    workspace_terraform = var.create_workspace_terraform_variable > 0 ? scalr_variable.workspace_terraform[0].value : "not created"
+    account_shell       = var.create_account_shell_variable > 0 ? scalr_variable.account_shell[0].readable_value : "not created"
+    environment_shell   = var.create_environment_shell_variable > 0 ? scalr_variable.environment_shell[0].readable_value : "not created"
+    workspace_shell     = var.create_workspace_shell_variable > 0 ? scalr_variable.workspace_shell[0].readable_value : "not created"
+    workspace_terraform = var.create_workspace_terraform_variable > 0 ? scalr_variable.workspace_terraform[0].readable_value : "not created"
   }
 }
