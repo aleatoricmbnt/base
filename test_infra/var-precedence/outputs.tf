d5fa8e4667d7cc5b355f2b74cae38f94e1d30aa0
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-07-16 17:07:48 +0300

    test plan with infra to deploy // test 2

--- test_infra/var-precedence/outputs.tf
+++ test_infra/var-precedence/outputs.tf
@@ -7,13 +7,3 @@ output "workspace_name" {
   description = "Name of the created test workspace"
   value       = scalr_workspace.var_precedence_test.name
 }
-
-output "variables_created" {
-  description = "Variables created for precedence testing"
-  value = {
-    account_shell     = scalr_variable.account_shell.value
-    environment_shell = scalr_variable.environment_shell.value
-    workspace_shell   = scalr_variable.workspace_shell.value
-    workspace_terraform = scalr_variable.workspace_terraform.value
-  }
-} 
