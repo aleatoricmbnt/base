2b5094dbd06414cd43b2c149b14fe6cd59e4c54d
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-07-28 15:07:31 +0300

    temp

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,4 +1,9 @@
-module "modules_some-long-module-name-without-terraform-prefix-to-test-the-dropdown" {
-	source  = "aleatoric.main.scalr.dev/Namespace_with_underscores_and_dashes-/modules/test//modules/some-long-module-name-without-terraform-prefix-to-test-the-dropdown"
+module "modules_terraform-empty-readme" {
+	source  = "aleatoric.main.scalr.dev/demo-terraform-infrastructure-config-with-monitoring-and-backups-v1-70/modules/test//modules/terraform-empty-readme"
 	version = "1.0.2"
+
+	# Set 1 required variable below.
+
+	# Number of resources to be created
+ 	quantity = 1
 }
