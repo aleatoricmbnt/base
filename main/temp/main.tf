bdd8a9e794d8445783cceb9984adffc852a9c7e2
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-07-28 15:20:21 +0300

    temp

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,5 +1,5 @@
 module "modules_terraform-empty-readme" {
-	source  = "aleatoric.main.scalr.dev/demo-terraform-infrastructure-config-with-monitoring-and-backups-v1-70/modules/test//modules/terraform-empty-readme"
+	source  = "aleatoric.main.scalr.dev/my_very_first_namespace/modules/test//modules/terraform-empty-readme"
 	version = "1.0.2"
 
 	# Set 1 required variable below.
