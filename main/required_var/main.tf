259ce83f9e6c54f3f42e0de2bc8309a448b0b980
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-07-28 11:49:48 +0300

    Added terraform.tfvars.json to the required_var plan

--- main/required_var/main.tf
+++ main/required_var/main.tf
@@ -1,9 +1,13 @@
 resource "terraform_data" "this" {
-  input = var.input
+  input = [ var.input, var.input2 ]
   triggers_replace = var.triggers_replace
 }
 
 variable "input" {
+  type = string
+}
+
+variable "input2" {
   type = string
 }
 
