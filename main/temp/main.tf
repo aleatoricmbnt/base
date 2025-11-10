706bd9456dcbdbcd7ee622161b4805a715c8e9a0
aleatoricmbnt <aleatoricmbnt@gmail.com>
2025-11-10 11:24:44 +0200

    test vp

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,4 +1,7 @@
 resource "terraform_data" "this" {
-  input = [ "kek", "var.input, var.input2", "kek" ]
+  input = {
+    key1 = "kek"
+    key4 = "lol"
+  }
   triggers_replace = "2025-11-10T09:13:16Z"
 }
