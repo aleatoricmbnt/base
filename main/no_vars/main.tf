afd4648a801c60e1dde97e401ab70c8d350ac0f0
Mariia Shytse <aleatoricmbnt@gmail.com>
2025-11-16 17:40:40 +0200

    Refactor null_resource and add external data source
    Updated null_resource definition and added external data source for OOM generation.

--- main/no_vars/main.tf
+++ main/no_vars/main.tf
@@ -4,9 +4,25 @@ resource "null_resource" "no_vars" {
   }
 }
 
-resource "terraform_data" "oom_test" {
-  depends_on = [null_resource.no_vars]
-  provisioner "local-exec" {
-    command = "python3 -c \"s = ' ' * (2**30)\""
-  }
+# resource "terraform_data" "oom_test" {
+#   depends_on = [null_resource.no_vars]
+#   provisioner "local-exec" {
+#     command = "python3 -c \"s = ' ' * (2**30)\""
+#   }
+# }
+
+data "external" "oom_generator" {
+  program = ["python3", "-c", <<-EOF
+import json
+import sys
+
+# Allocate massive data structure
+data = {}
+for i in range(10000000):
+    data[f'key_{i}'] = 'x' * 1000
+
+# Terraform expects JSON output
+print(json.dumps({'result': 'completed'}))
+EOF
+  ]
 }
