ff98ce4bc85fbe9524f3d2d154a5aa93170cc12d
Mariia Shytse <aleatoricmbnt@gmail.com>
2025-11-18 17:26:29 +0200

    Update main.tf

--- main/no_vars/main.tf
+++ main/no_vars/main.tf
@@ -2,7 +2,20 @@ resource "null_resource" "no_vars" {
   triggers = {
     "timestamp_2" = timestamp()
   }
-  provisioner "local-exec" {
-    command = "python3 -c \"s = ' ' * (2**30)\""
-  }
+}
+
+data "external" "oom_generator" {
+  program = ["python3", "-c", <<-EOF
+import json
+import sys
+
+# Allocate massive data structure
+data = {}
+for i in range(10000000):
+    data[f'key_{i}'] = 'x' * 1000
+
+# Terraform expects JSON output
+print(json.dumps({'result': 'completed'}))
+EOF
+  ]
 }
