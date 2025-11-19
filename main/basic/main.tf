3e6dff2d9123434501b948d72014075563046615
Mariia Shytse <aleatoricmbnt@gmail.com>
2025-11-19 16:22:12 +0200

    Update main.tf

--- main/basic/main.tf
+++ main/basic/main.tf
@@ -22,18 +22,9 @@ resource "terraform_data" "test" {
   ]
 }
 
-output "resource_summary" {
-  value = {
-    total_resources = length(resource.terraform_data.test)
-    sample_outputs = slice([
-      for r in resource.terraform_data.test : r.input
-    ], 0, 10)
-  }
-
-}
-
 resource "terraform_data" "test2" {
   input = var.input
   triggers_replace = timestamp()
 }
+
 
