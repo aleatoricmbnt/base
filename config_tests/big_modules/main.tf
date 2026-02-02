edea1b4242f6eb58d38ab1410192f7d72c387b90
aleatoricmbnt <aleatoricmbnt@gmail.com>
2026-02-02 20:24:22 +0200

    multiple big local modules

--- config_tests/big_modules/main.tf
+++ config_tests/big_modules/main.tf
@@ -1,7 +1,59 @@
-module "big_plan" {
-  source = "../big_plan"
+module "root_01" {
+  source = "../.."
 }
 
-module "root" {
+module "root_02" {
+  source = "../.."
+}
+
+module "root_03" {
+  source = "../.."
+}
+
+module "root_04" {
+  source = "../.."
+}
+
+module "root_05" {
+  source = "../.."
+}
+
+module "root_06" {
+  source = "../.."
+}
+
+module "root_07" {
+  source = "../.."
+}
+
+module "root_08" {
+  source = "../.."
+}
+
+module "root_09" {
+  source = "../.."
+}
+
+module "root_10" {
+  source = "../.."
+}
+
+module "root_11" {
+  source = "../.."
+}
+
+module "root_12" {
+  source = "../.."
+}
+
+module "root_13" {
+  source = "../.."
+}
+
+module "root_14" {
+  source = "../.."
+}
+
+module "root_15" {
   source = "../.."
 }
