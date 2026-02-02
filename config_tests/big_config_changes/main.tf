63779f9dfae39dd26610d9ba7276a7923bda3a35
aleatoricmbnt <aleatoricmbnt@gmail.com>
2026-02-02 21:14:00 +0200

    back to basic

--- config_tests/big_config_changes/main.tf
+++ config_tests/big_config_changes/main.tf
@@ -1,31 +1,3 @@
-module "root_01" {
-  source = "../.."
-}
-
-module "root_02" {
-  source = "../.."
-}
-
-module "root_03" {
-  source = "../.."
-}
-
-module "root_04" {
-  source = "../.."
-}
-
-module "root_05" {
-  source = "../.."
-}
-
-module "root_06" {
-  source = "../.."
-}
-
-module "root_07" {
-  source = "../.."
-}
-
-module "root_08" {
-  source = "../.."
+resource "terraform_data" "test" {
+  input = "test"
 }
