bafce2a2084a952209b69cb59442e34cdcd14979
aleatoricmbnt <aleatoricmbnt@gmail.com>
2026-02-02 20:13:29 +0200

    big plan update

--- config_tests/big_plan/main.tf
+++ config_tests/big_plan/main.tf
@@ -1,27 +1,47 @@
 terraform {
   required_providers {
-    # Multiple providers = larger .terraform directory
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
-    google = { source = "hashicorp/google", version = "~> 5.0" }
-    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.0" }
+    # Cloud providers (large binaries)
+    aws        = { source = "hashicorp/aws", version = "~> 5.0" }        # ~400MB
+    google     = { source = "hashicorp/google", version = "~> 5.0" }     # ~200MB
+    azurerm    = { source = "hashicorp/azurerm", version = "~> 3.0" }    # ~300MB
+    
+    # Kubernetes ecosystem (large)
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.0" }  # ~100MB
+    helm       = { source = "hashicorp/helm", version = "~> 2.0" }        # ~80MB
+    
+    # Other HashiCorp products
+    vault      = { source = "hashicorp/vault", version = "~> 3.0" }       # ~50MB
+    consul     = { source = "hashicorp/consul", version = "~> 2.0" }      # ~50MB
+    nomad      = { source = "hashicorp/nomad", version = "~> 2.0" }       # ~50MB
+    
+    # Popular third-party providers
+    datadog    = { source = "datadog/datadog", version = "~> 3.0" }       # ~100MB
+    newrelic   = { source = "newrelic/newrelic", version = "~> 3.0" }     # ~50MB
+    cloudflare = { source = "cloudflare/cloudflare", version = "~> 4.0" } # ~80MB
+    github     = { source = "integrations/github", version = "~> 5.0" }   # ~50MB
+    gitlab     = { source = "gitlabhq/gitlab", version = "~> 16.0" }      # ~60MB
+    
+    # Database providers
+    postgresql = { source = "cyrilgdn/postgresql", version = "~> 1.0" }   # ~30MB
+    mysql      = { source = "petoju/mysql", version = "~> 3.0" }          # ~20MB
+    
+    # Other cloud providers
+    oci        = { source = "oracle/oci", version = "~> 5.0" }            # ~200MB
+    alicloud   = { source = "aliyun/alicloud", version = "~> 1.0" }       # ~100MB
+    
+    # Utility providers (smaller but still add up)
+    random     = { source = "hashicorp/random", version = "~> 3.0" }
+    null       = { source = "hashicorp/null", version = "~> 3.0" }
+    local      = { source = "hashicorp/local", version = "~> 2.0" }
+    tls        = { source = "hashicorp/tls", version = "~> 4.0" }
+    http       = { source = "hashicorp/http", version = "~> 3.0" }
   }
 }
 
-resource "null_resource" "create_large_files" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      # Create 100MB of random files in subdirectories
-      for i in {1..100}; do
-        mkdir -p large_data_$i
-        dd if=/dev/urandom of=large_data_$i/file_$i.bin bs=1M count=1
-      done
-    EOT
-  }
+locals {
+  large_data = templatefile("${path.module}/large_template.tpl", {})
 }
 
-resource "random_id" "test" {
-  byte_length = 4
-  depends_on = [null_resource.create_large_files]
+resource "terraform_data" "test" {
+  input = local.large_data
 }
-
