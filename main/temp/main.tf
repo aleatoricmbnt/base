8d0a429dd6be8eb6dedc0e8ad101e7694a9170aa
aleatoricmbnt <aleatoricmbnt@gmail.com>
2026-02-02 14:19:47 +0200

    check long running run

--- main/temp/main.tf
+++ main/temp/main.tf
@@ -1,8 +1,4 @@
 resource "terraform_data" "this" {
-  input = {
-    key1 = "kek"
-    key4 = "lol"
-    keys2 = "lolkek"
-  }
-  triggers_replace = "2025-11-10T09:13:16Z"
+  count = 10000
+  input = timestamp()
 }
