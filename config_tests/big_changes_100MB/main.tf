01d86790662a29e989ca6aa24cffb2bd8e96f8df
aleatoricmbnt <aleatoricmbnt@gmail.com>
2026-02-04 16:04:00 +0200

    1 module check size

---
+++ config_tests/big_changes_100MB/main.tf
@@ -0,0 +1,7 @@
+module "no_vars" {
+  source = "git::https://github.com/aleatoricmbnt/base.git//main/no_vars?ref=master"
+}
+
+module "readme_links" {
+  source = "git::https://github.com/aleatoricmbnt/base.git//main/readme_links?ref=master"
+}
