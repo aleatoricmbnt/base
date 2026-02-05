0716f16091f2e76141434f84610070f3a40ffed5
aleatoricmbnt <aleatoricmbnt@gmail.com>
2026-02-05 21:14:17 +0200

    add module

--- config_tests/module_sources/module_github_com_with_subdir/main.tf
+++ config_tests/module_sources/module_github_com_with_subdir/main.tf
@@ -1,3 +1,8 @@
 module "referencing" {
   source = "github.com/aleatoricmbnt/base.git//config_tests/module_sources/module_github_com"
 }
+
+
+module "smth_from_git" {
+  source = "github.com/aleatoricmbnt/flat/"
+}
