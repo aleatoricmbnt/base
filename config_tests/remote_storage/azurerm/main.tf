020ea076c320b44c25430304323517d08cc8d69c
aleatoricmbnt <aleatoricmbnt@gmail.com>
2026-02-25 22:17:58 +0200

    add remote state azure + outputs

--- config_tests/remote_storage/azurerm/main.tf
+++ config_tests/remote_storage/azurerm/main.tf
@@ -68,3 +68,83 @@ output "long_string_attribute" {
   description = "The longest string attribute for testing output formatting"
   value       = var.attributes.random_value_random_value_value_test_value_random_function_underscore_function_random_generator_test_python_string_function
 }
+
+data "terraform_remote_state" "rs_tf-var-types" {
+  backend = "remote"
+
+  config = {
+    hostname = "test.mshytse-tests.testenv.scalr.dev"
+    organization = "env-v0p5qmic8hdkh0m1p"
+    workspaces = {
+      name = "tf-var-types"
+    }
+  }
+}
+
+locals {
+  non_sensitive_list_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_list_output
+  non_sensitive_list_untyped_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_list_untyped_output
+  non_sensitive_map_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_map_output
+  non_sensitive_map_untyped_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_map_untyped_output
+  non_sensitive_object_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_object_output
+  non_sensitive_object_untyped_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_object_untyped_output
+  non_sensitive_set_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_set_output
+  non_sensitive_set_untyped_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_set_untyped_output
+  non_sensitive_simple_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_simple_output
+  non_sensitive_tuple_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_tuple_output
+  non_sensitive_tuple_untyped_output = data.terraform_remote_state.rs_tf-var-types.outputs.non_sensitive_tuple_untyped_output
+  sensitive_output = data.terraform_remote_state.rs_tf-var-types.outputs.sensitive_output
+}
+
+resource "terraform_data" "this" {
+  input = timestamp()
+}
+
+output "non_sensitive_list_output" {
+  value = local.non_sensitive_list_output
+}
+
+output "non_sensitive_list_untyped_output" {
+  value = local.non_sensitive_list_untyped_output
+}
+
+output "non_sensitive_map_output" {
+  value = local.non_sensitive_map_output
+}
+
+output "non_sensitive_map_untyped_output" {
+  value = local.non_sensitive_map_untyped_output
+}
+
+output "non_sensitive_object_output" {
+  value = local.non_sensitive_object_output
+}
+
+output "non_sensitive_object_untyped_output" {
+  value = local.non_sensitive_object_untyped_output
+}
+
+output "non_sensitive_set_output" {
+  value = local.non_sensitive_set_output
+}
+
+output "non_sensitive_set_untyped_output" {
+  value = local.non_sensitive_set_untyped_output
+}
+
+output "non_sensitive_simple_output" {
+  value = local.non_sensitive_simple_output
+}
+
+output "non_sensitive_tuple_output" {
+  value = local.non_sensitive_tuple_output
+}
+
+output "non_sensitive_tuple_untyped_output" {
+  value = local.non_sensitive_tuple_untyped_output
+}
+
+output "sensitive_output" {
+  value     = local.sensitive_output
+}
+
