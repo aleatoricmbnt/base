e698dca0974ae408807e625a4692456f11fb1463
aleatoricmbnt <aleatoricmbnt@gmail.com>
2026-04-28 21:29:18 +0300

    test billing

--- config/negative/fmt_failed/main.tf
+++ config/negative/fmt_failed/main.tf
@@ -2,3 +2,9 @@ resource "terraform_data" "x" {
   input = "value"
   triggers_replace = [ timestamp() ]
 }
+
+resource "null_resource" "x" {
+   triggers = {   # three spaces instead of two
+      key = "value"
+   }
+}
