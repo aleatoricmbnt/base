9d8380f154fe373ce9cf8d5642a708248fa6e24b
aleatoricmbnt <aleatoricmbnt@gmail.com>
2026-04-29 14:55:57 +0300

    invalid plugin config

---
+++ main/tf_lab/invalid_plugin_config/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "vault" {
+  address = "http://255.255.255.255:8200"
+  token   = "fake-token"
+}
+
+resource "vault_generic_secret" "test" {
+  path      = "secret/billing-test"
+  data_json = jsonencode({ key = "value" })
+}
